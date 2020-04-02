6d2f3877660043bd9a99ac2616440f83dc598929
alexfurmenkov <alexfurm@mail.ru>
2020-04-02 17:16:20 +0200

    [UPDATE] Add routes for static files

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,21 @@
-from src.views.home import home
+from src.views.home import Home
 from src.views.signup import Signup
 from src.views.login import Login
 from src.middlewares.get_user import get_user
 
-routes = [
-    ('GET', '/', home),
+
+url_routes = [
+    ('GET', '/', Home),
     ('GET', '/user/', get_user),
     ('POST', '/signup/', Signup),
+    ('GET', '/signup.html', Signup),
     ('POST', '/login/', Login),
+    ('GET', '/login.html', Login),
+]
+
+static_routes = [
+    ('/css/', 'static/css', 'css'),
+    ('/fonts/', 'static/fonts', 'fonts'),
+    ('/img/', 'static/img', 'img'),
+    ('/js/', 'static/js', 'js'),
 ]
--- src/server.py
+++ src/server.py
@@ -3,16 +3,19 @@ import jinja2
 import aiohttp_jinja2
 import asyncio
 from aiohttp import web
-from src.routes import routes
+from src.routes import url_routes, static_routes
 from src.middlewares.auth import auth_middleware
 
 
 loop = asyncio.get_event_loop()
 server = web.Application(loop=loop, middlewares=[auth_middleware])
-TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'static')
 
-for route in routes:
-    server.router.add_route(route[0], route[1], route[2])
+for static_route in static_routes:
+    server.router.add_static(static_route[0], path=static_route[1], name=static_route[2])
+
+for url_route in url_routes:
+    server.router.add_route(url_route[0], url_route[1], url_route[2])
 
 aiohttp_jinja2.setup(server, loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
 
