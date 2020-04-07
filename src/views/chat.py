0c625d8bb0c7d8d69dfe941d8d00f32fcb351bdd
alexfurmenkov <alexfurm@mail.ru>
2020-04-07 15:18:02 +0200

    [UPDATE] Return all messages from DB in src/views/chat.py

--- src/views/chat.py
+++ src/views/chat.py
@@ -1,13 +1,17 @@
 from aiohttp import web
 import aiohttp_jinja2
 from src.decorators.login_required import login_required
+from src.models.message import Message
+from src.db_settings import objects
 
 
 class Chat(web.View):
     @login_required
     @aiohttp_jinja2.template('chat.html')
     async def get(self):
+        messages = await objects.execute(Message.select())
         return dict(
             status='online',
             button_text='Logout',
+            messages=messages,
         )
