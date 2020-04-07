75882a7b891ffa8347e64903a0174ce785fe28e8
alexfurmenkov <alexfurm@mail.ru>
2020-04-07 15:19:04 +0200

    [UPDATE] Create new model: Message

--- src/db_settings.py
+++ src/db_settings.py
@@ -1,8 +1,9 @@
 import peewee_async
 from src.models.user import database, User
+from src.models.message import Message
 
 
 database.connect()
-database.create_tables([User])
+database.create_tables([User, Message])
 database.close()
 objects = peewee_async.Manager(database)
