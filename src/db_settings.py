301cbecfef1e7b31e8c9b95ee238214814dad6a1
alexfurmenkov <alexfurm@mail.ru>
2020-04-07 16:56:10 +0200

    [UPGRADE] Make model imports more elegant

--- src/db_settings.py
+++ src/db_settings.py
@@ -1,8 +1,6 @@
 import peewee_async
 from src.db_data import database
-from src.models.base import BaseModel
-from src.models.user import User
-from src.models.message import Message
+from src.models import *
 
 
 database.connect()
